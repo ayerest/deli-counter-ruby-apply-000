2ccfb055c7c05c0c593f8a3e18378f24ba8bc989
Iris S <irisiseve@gmail.com>
2019-05-16 20:45:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,7 @@
 # Write your code here.
 
+counter = 0
+
 def line(deli)
   if deli.empty?
     puts "The line is currently empty."
@@ -27,8 +29,9 @@ end
 # end
 
 def take_a_number(deli_line, name)
+  counter += 1
   deli_line << name
-  puts "Welcome, #{name}. You are number #{deli_line.length} in line."
+  puts "Welcome, #{name}. You are ticket number #{counter}."
 end
 
 
@@ -36,7 +39,7 @@ def now_serving(deli_line)
   if deli_line.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{deli_line.first}."
+    puts "Currently serving ticket number #{counter}."
     deli_line.shift()
   end
 end
